2aab5a0d7590ef4e0ba2fe38c6c50159a7f5b718
celebrate325 <439724550@qq.com>
2020-04-15 12:56:22 +0800

    第一次提交代码

---
+++ pages/demo/demo.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<view class="view-box animate a-swing" hover-stay-time="800" hover-class="view-box-hover"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.view-box{
+		width: 200upx;
+		height: 200upx;
+		background: #CCCCCC;
+		color:#007AFF;
+		margin: 100upx auto;
+	}
+	
+	.view-box-hover{
+		background: red;
+	}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<view class="icon iconfont icon-home" style="color: red;font-size: 100upx;"></view>
+		<navigator url="../demo/demo">
+			<view>跳转到测试页</view>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.view-box{
+		width: 200upx;
+		height: 200upx;
+		background: #CCCCCC;
+		color:#007AFF;
+		margin: 100upx auto;
+	}
+	
+	.view-box-hover{
+		background: red;
+	}
+</style>
